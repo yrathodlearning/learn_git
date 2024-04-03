e23577522a8a768c4b78b684b207f04a5385d45c
yrathodlearning <yrathodlearning@gmail.com>
2024-04-03 17:13:55 +0530

    My Second chnages

---
+++ git_command.sh
@@ -0,0 +1,6 @@
+git config --global user.name "username" # add username in configuration
+git config --global user.email "email" # 
+
+git add "File Name" # add file at a time
+git commit -m "Commit Message" # Commit all the staged changes
+git add . #add all the changes
